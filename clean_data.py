5b594e33c08f49fb5118c8778300269dfabc2dd7
Rmblai02 <you@example.com>
2023-06-06 19:21:44 -0500

    Added Git Hub codes for Reference

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,19 @@
 #this is just a test
 
-
+## Create a file, called clean_data.py
+#touch clean_data.py
+# Add and commit clean_data.py...
+#git add clean_data.py
+#git status
+#git commit -m "First commit."
+# Add cleanup code to clean_data.py...
+#git add clean_data.py
+#git status
+#git commit -m "Clean up provided data."
+# Add code to export clean data...Note that add . adds
+# everything in the current folder
+#git add .
+#git status
+#git commit -m "Export clean data as CSV."
 
 #this is cleaned up 
